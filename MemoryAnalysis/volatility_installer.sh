16a504638d70fa0c24a7658925ecda64c7c65627
TheOffensiveDefender <154365851+TheOffensiveDefender@users.noreply.github.com>
2023-12-27 19:12:00 -0800

    Update volatility_installer.sh

--- MemoryAnalysis/volatility_installer.sh
+++ MemoryAnalysis/volatility_installer.sh
@@ -21,7 +21,7 @@ DEBIAN=""
 
 # Check if running under elevated permissions
 if [ "$EUID" -ne 0 ]; then
-	print "[!] Please run this script elevated with either sudo permissions or run as the root user"
+	printf "[!] Please run this script elevated with either sudo permissions or run as the root user"
 	exit
 fi
 
@@ -34,35 +34,35 @@ fi
 
 # Run commands to install Volatility 
 if [[ DEBIAN = true ]]; then
-	print "[*] Updating APT repository package lists\n"
+	printf "[*] Updating APT repository package lists\n"
 	apt-get update
-	print "[*] Installing Python3, PIP, and Git\n"
+	printf "[*] Installing Python3, PIP, and Git\n"
 	apt install -y python3 python3-pip git
-	print "[*] Cloning Git repository $VOLATILITYURL to folder $INSTALLDIR\n"
+	printf "[*] Cloning Git repository $VOLATILITYURL to folder $INSTALLDIR\n"
 	git clone $VOLATILITYURL $INSTALLDIR
-	print "[*] Giving "execute" permissions to all Python files in $INSTALLDIR\n"
+	printf "[*] Giving "execute" permissions to all Python files in $INSTALLDIR\n"
 	find $INSTALLDIR -type f -name "*.py" -exec chmod +x {} \;
-	print "[*] Upgrading Python 3 PIP installation\n"
+	printf "[*] Upgrading Python 3 PIP installation\n"
 	python3 -m pip install --upgrade pip
-	print "[*] Installing Volatility 3 PIP requirements from file $VOLREQS\n"
+	printf "[*] Installing Volatility 3 PIP requirements from file ${VOLREQS}\n"
 	python3 -m pip install -r $VOLREQS
-	print "[*] Creating a symbolic link to $VOLBIN in $SYMLINKLOC\n"
+	printf "[*] Creating a symbolic link to ${VOLBIN} in ${SYMLINKLOC}\n"
 	ln -s $VOLBIN /bin/vol
 else
-	print "[*] Updating YUM repository package lists\n"
+	printf "[*] Updating YUM repository package lists\n"
 	yum update -y
-	print "[*] Installing Python 3, PIP, and Git\n"
+	printf "[*] Installing Python 3, PIP, and Git\n"
 	yum install -y python3 python3-pip git
-	print "[*] Cloning Git repository $VOLATILITYURL to folder $INSTALLDIR\n"
+	printf "[*] Cloning Git repository $VOLATILITYURL to folder ${INSTALLDIR}\n"
 	git clone $VOLATILITYURL $INSTALLDIR
-	print "[*] Giving "execute" permissions to all Python files in $INSTALLDIR\n"
+	printf "[*] Giving "execute" permissions to all Python files in ${INSTALLDIR}\n"
 	find $INSTALLDIR -type f -name "*.py" -exec chmod +x {} \;
-	print "[*] Upgrading Python 3 PIP installation\n"
+	printf "[*] Upgrading Python 3 PIP installation\n"
 	python3 -m pip install --upgrade pip
-	print "[*] Installing Volatility 3 PIP requirements from file $VOLREQS\n"
+	printf "[*] Installing Volatility 3 PIP requirements from file ${VOLREQS}\n"
 	python3 -m pip install -r $VOLREQS
-	print "[*] Creating a symbolic link to $VOLBIN in $SYMLINKLOC\n"
+	printf "[*] Creating a symbolic link to ${VOLBIN} in ${SYMLINKLOC}\n"
 	ln -s $VOLBIN /bin/vol
 fi
 
-print "\n[*] Script finished"
+printf "\n[*] Script finished"
